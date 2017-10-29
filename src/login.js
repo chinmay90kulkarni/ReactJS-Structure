fce74f2c51d133396fc9cad54357f9c33de21d1a
Chinmay Kulkarni <chinmay90kulkarni@gmail.com>
2017-10-29 20:10:56 +0530

    Add files via upload

---
+++ src/login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './login.css';
+
+class Login extends Component {
+  render() {
+    return (
+      <div id="hd">
+      </div>
+    );
+  }
+}
+
+export default Login;
